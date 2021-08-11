6e9407ada9301780f18a4866dc4e2004b639d176
komekome09 <fanmaskome@gmail.com>
2021-08-11 11:33:46 +0900

    Remove unused code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-extern crate combine;
-
-use combine::{Parser, many1};
-use combine::parser::char::digit;
-
-fn main() {
-    let mut parser = many1::<Vec<_>, _, _>(digit());
-    println!("{:?}", parser.parse("123"));
-    println!("{:?}", parser.parse("123ABC"));
-    println!("{:?}", parser.parse("ABC123"));
-}
+fn main(){}
